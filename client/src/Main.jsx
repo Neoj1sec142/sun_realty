fe00b93d181e98d3bf83f9b34fde99643d97de3d
Mark Harmon <neothej1sec@gmail.com>
2022-09-15 23:22:51 -0500

    found how to export as csv

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
 // import { render } from 'react-dom';
 import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
-
 import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
 import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+
 
 const Main = () => {
     const gridRef = useRef(); // Optional - for accessing Grid's API
@@ -38,10 +38,18 @@ const Main = () => {
         gridRef.current.api.deselectAll();
     }, []);
 
+    
+    const onBtnUpdate = useCallback(() => {
+        // document.querySelector(
+        //   '#csvResult'
+        // ).value = 
+        console.log(gridRef.current.api.getDataAsCsv())
+      }, []);
+   
     return (
         <div>
             {/* Example using Grid's API */}
-            <button onClick={buttonListener}>Push Me</button>
+            <button onClick={onBtnUpdate}>Show CSV export content text</button>
     
             {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
             <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
@@ -50,7 +58,7 @@ const Main = () => {
                 ref={gridRef} // Ref for accessing Grid's API
     
                 rowData={rowData} // Row Data for Rows
-    
+                reactUi={true}
                 columnDefs={columnDefs} // Column Defs for Columns
                 defaultColDef={defaultColDef} // Default Column Properties
     
