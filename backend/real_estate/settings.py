c20c6deb34cdfc229ed498930a6322d02f259497
Mark Harmon <neothej1sec@gmail.com>
2022-10-20 13:31:29 -0500

    created accounts app

--- backend/real_estate/settings.py
+++ backend/real_estate/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 
 FILE_UPLOAD_PERMISSIONS = 0o640
-
+AUTH_USER_MODEL = 'accounts.UserAccount'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- backend/real_estate/urls.py
+++ backend/real_estate/urls.py
@@ -13,7 +13,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-] 
-urlpatterns += [re_path(r'^.*'), TemplateView.as_view(template_name='index.html')]
-if settings.DEBUG:
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
