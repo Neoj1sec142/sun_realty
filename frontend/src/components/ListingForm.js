55c27f2f3a61f6acb3ad667d863665ebaf3efe8c
Mark Harmon <neothej1sec@gmail.com>
2022-10-23 15:32:03 -0500

    set up listing form

--- frontend/src/components/ListingForm.js
+++ frontend/src/components/ListingForm.js
@@ -46,7 +46,125 @@ const ListingForm = (props) => {
     }
 
     return(
-        <div></div>
+        <form className='listingform' onSubmit={e=>onSubmit(e)}>
+            <div className='row'>
+                <div className='col-1-of-6'>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='sale_type'>Sale or Rent</label>
+                        <select className='listingform__select' name='sale_type' onChange={e=>onChange(e)} value={sale_type}>
+                            <option>For Sale</option>
+                            <option>For Rent</option>
+                        </select>
+                    </div>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='sqft'>Sqft:</label>
+                        <select className='listingform__select' name='sqft' onChange={e=>onChange(e)} value={sqft}>
+                            <option>1000+</option>
+                            <option>1200+</option>
+                            <option>1500+</option>
+                            <option>2000+</option>
+                            <option>Any</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='col-1-of-6'>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='price'>Minimum Proce:</label>
+                        <select className='listingform__select' name='price' onChange={e=>onChange(e)} value={price}>
+                            <option>$0+</option>
+                            <option>$200,000+</option>
+                            <option>$400,000+</option>
+                            <option>$600,000+</option>
+                            <option>$800,000+</option>
+                            <option>$1,000,000+</option>
+                            <option>$1,200,000+</option>
+                            <option>$1,500,000+</option>
+                            <option>Any</option>
+                        </select>
+                    </div>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='days_listed'>Days Listed:</label>
+                        <select className='listingform__select' name='days_listed' onChange={e=>onChange(e)} value={days_listed}>
+                            <option>1 or less</option>
+                            <option>2 or less</option>
+                            <option>5 or less</option>
+                            <option>10 or less</option>
+                            <option>20 or less</option>
+                            <option>Any</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='col-1-of-6'>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='bedrooms'>Bedrooms:</label>
+                        <select className='listingform__select' name='bedrooms' onChange={e=>onChange(e)} value={bedrooms}>
+                            <option>0+</option>
+                            <option>1+</option>
+                            <option>2+</option>
+                            <option>3+</option>
+                            <option>4+</option>
+                            <option>5+</option>
+                        </select>
+                    </div>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='has_photos'>Has Photos:</label>
+                        <select className='listingform__select' name='has_photos' onChange={e=>onChange(e)} value={has_photos}>
+                            <option>1+</option>
+                            <option>3+</option>
+                            <option>5+</option>
+                            <option>10+</option>
+                            <option>15+</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='col-1-of-6'>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='home_type'>Home Type:</label>
+                        <select className='listingform__select' name='home_type' onChange={e=>onChange(e)} value={home_type}>
+                            <option>House</option>
+                            <option>Condo</option>
+                            <option>Townhouse</option>
+                        </select>
+                    </div>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='has_photos'>Keywords:</label>
+                        <input className='listingform__input' name='keywords' type='text' onChange={e=>onChange(e)} value={keywords}/>
+                    </div>
+                </div>
+                <div className='col-1-of-6'>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='bathrooms'>Baths:</label>
+                        <select className='listingform__select' name='bathrooms' onChange={e=>onChange(e)} value={bathrooms}>
+                            <option>0+</option>
+                            <option>1+</option>
+                            <option>2+</option>
+                            <option>3+</option>
+                            <option>4+</option>
+                        </select>
+                    </div>
+                    <div className='listingform__section'>
+                        <label className='listingform__label' htmlFor='open_house'>Open Houses:</label>
+                        <input className='listingform__checkbox' name='open_house' type='checkbox' onChange={e=>onChange(e)} value={open_house}/>
+                    </div>
+                </div>
+                <div className='col-1-of-6'>
+                    {loading ?
+                        <div className='listingform__loader'>
+                            <Loader 
+                                type='Oval'
+                                color='424242'
+                                height='50'
+                                width='50'
+                            />
+                        </div> : <button className='listingform__button listingform__button--primary'>Save</button>
+                    }
+                </div>
+            </div>
+        </form>
     )
 }
+ListingForm.propTypes ={
+    setListings: PropTypes.func.isRequired
+}
+
 export default ListingForm
